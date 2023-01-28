7d00a1fda581d8e51d9c6b1feaf3cf8237c5140c
YEJI <yedoong919@gmail.com>
2023-01-28 23:46:45 +0900

    0128_ 코드 수정하기

--- exam/src/ch3/Exercise3_7.java
+++ exam/src/ch3/Exercise3_7.java
@@ -8,7 +8,7 @@ class Exercise3_7 {
 	public static void main(String[] args) {
 		int fahrenheit = 100;
 		//float celcius = ( /* (1) */ );
-		float celcius = 5/9 *(fahrenheit - 32);
+		float celcius = 0f;
 		System.out.println("Fahrenheit:"+fahrenheit);
 		System.out.println("Celcius:"+celcius);
 	}
